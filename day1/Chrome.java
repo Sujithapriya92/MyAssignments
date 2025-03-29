7e47e84b14540bcd761c173991d0e704fff1ad99
Sujithapriya92 <Sujithapriya92@gmail.com>
2025-03-29 19:50:46 +0530

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version=91.0f;
+		System.out.println(version);
+		
+		String name="Google";
+		System.out.println(name);
+		
+		boolean isBeta =false;
+		System.out.println(isBeta);
+		
+		int releaseYear =2008;
+		System.out.println(releaseYear);
+		
+		char shortcutKey ='C';
+		System.out.println(shortcutKey);
+		
+
+	}
+
+}
